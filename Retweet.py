242710f5191b0a53e2e2339c8a5cc4264f2d9238
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-03 15:15:20 +0530

    Update Retweet.py

--- Retweet.py
+++ Retweet.py
@@ -7,12 +7,11 @@ from keys import *
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-searchTerm = input("Enter Keyword/Tag to search about: ")
-NoOfTerms = int(input("Enter how many tweets to search: "))
+
 # Where q='#example', change #example to whatever hashtag or keyword you want to search.
 # Where items(5), change 5 to the amount of retweets you want to tweet.
 # Make sure you read Twitter's rules on automation - don't spam!
-for tweet in tweepy.Cursor(api.search, q=searchTerm, -filter:retweets'), lang='en').items(NoOfTerms):
+for tweet in tweepy.Cursor(api.search, q=('#java OR #nasa OR #Aurdino -filter:retweets'), lang='en').items(500):
     try:
         print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
 
