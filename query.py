0678107442c43364d89884bb37d93b1bd225dbec
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-03 16:09:59 +0530

    Update query.py

--- query.py
+++ query.py
@@ -1,2 +1,3 @@
 print(Hello Friends Please only edit hastags and no you want to retweet)
 for tweet in tweepy.Cursor(api.search, q=('#java OR #nasa OR #Aurdino -filter:retweets'), lang='en').items(500):
+  try:
