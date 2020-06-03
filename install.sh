2cbe5506d8fd1723e8bdc2802c75717383930f63
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-03 20:02:08 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,187 @@
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#coding section starts :)
+clear
+echo " "
+echo " "
+echo -e "$red
+
+  _____   _______       _ _   _            ____        _   
+ |  __ \ |__   __|     (_) | | |          |  _ \      | |  
+ | |__) |___| |_      ___| |_| |_ ___ _ __| |_) | ___ | |_ 
+ |  _  // _ \ \ \ /\ / / | __| __/ _ \ '__|  _ < / _ \| __|
+ | | \ \  __/ |\ V  V /| | |_| ||  __/ |  | |_) | (_) | |_ 
+ |_|  \_\___|_| \_/\_/ |_|\__|\__\___|_|  |____/ \___/ \__|
+                                             v 1.1$rset"
+echo " "                                                          
+echo -e "$red▶ Coded by$grn🇵​​​​​🇷​​​​​🇦​​​​​🇱​​​​​🇭​​​​​🇦​​​​​🇩​​​​​$red ◀$rset"
+echo " "
+echo -e "$red⫸$ylo https://github.com/Pralhad-Nasane$red ⫷$rset"
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞$ylo [OPTIONS]$grn ＜＜＜＜＜＜＜$rset"
+echo " "
+echo " "
+echo " "
+echo " "
+echo -e "$cyan [1️⃣] Install Bot"
+echo -e "$cyan [2️⃣] Start Bot"
+echo -e "$cyan [3️⃣] Update ReTwitterBot"
+echo -e "$cyan [4️⃣] Delete ReTwitterBot"
+echo -e "$cyan [5️⃣] About Me"
+echo -e "$cyan [6️⃣] Exit"
+echo -e "$ylo Select any option :- $rset"
+read n
+case "$n" in
+1)echo " "
+echo " "
+echo -e "$grn 𝑯𝒆𝒍𝒍𝒐𝒘 𝑮𝒖𝒚𝒔 𝑻𝒐 𝑰𝑵𝑺𝑻𝑨𝑳𝑳 𝑩𝑶𝑻 𝒀𝒐𝒖 𝑯𝒂𝒗𝒆 𝒕𝒐 𝑭𝒊𝒓𝒔𝒕 𝑬𝒅𝒊𝒕 𝑺𝒐𝒎𝒆 𝑰𝒏𝒇𝒐𝒓𝒎𝒂𝒕𝒊𝒐𝒏 𝑨𝒔 𝑷𝒆𝒓 𝑰𝒏𝒔𝒕𝒓𝒖𝒄𝒕𝒊𝒐𝒏
+echo ""
+echo -e "$ylo 
+ 𝟣) 𝖤𝗇𝗍𝖾𝗋 𝖠𝗅𝗅 𝖸𝗈𝗎𝗋 𝖲𝗈𝗎𝗋𝖼𝖾 𝖪𝖾𝗒 𝗂𝗇 𝗅𝖾𝗒𝗌.𝗉𝗒 𝖿𝗂𝗅𝖾
+
+ 𝟤) 𝖠𝖿𝗍𝖾𝗋 𝖳𝗁𝖺𝗍 𝖾𝖽𝗂𝗍 𝖱𝖾𝗍𝗐𝖾𝖾𝗍.𝗉𝗒 𝖿𝗂𝗅𝖾 
+
+ 𝟥) 𝖤𝗇𝗍𝖾𝗋 𝗒𝗈𝗎𝗋 𝖧𝖺𝗌𝗍𝖺𝗀𝗌 𝗂𝗇 𝖱𝖾𝗍𝗐𝖾𝖾𝗍.𝗉𝗒 𝖿𝗂𝗅𝖾
+
+ 𝟦) 𝖠𝗅𝗅 𝖽𝖾𝗍𝖺𝗂𝗅 𝖥𝗈𝗋 𝖤𝖽𝗂𝗍𝗂𝗇𝗀 𝖺𝗋𝖾 𝖦𝗂𝗏𝖾𝗇 𝖨𝗇 𝖥𝗂𝗅𝖾 𝖳𝗈𝗈.
+
+ 𝟧) 𝖸𝗈𝗎 𝖢𝖺𝗇 𝖮𝗉𝖾𝗇 𝖳𝗁𝗂𝗌 𝖠𝗅𝗅 𝖥𝗂𝗅𝖾 𝖡𝗒 𝖩𝗎𝗌𝗍 𝖤𝗇𝗍𝖾𝗋𝗂𝗇𝗀 𝗇𝖺𝗇𝗈 𝖿𝗂𝗅𝖾𝗇𝖺𝗆𝖾
+ 
+ 𝟨) 𝖮𝗋 𝖸𝗈𝗎 𝖢𝖺𝗇 𝖱𝖾𝖺𝖽 𝖠𝗅𝗅 𝖯𝗋𝗈𝖼𝖾𝗌𝗌 𝗂𝗇 𝗋𝖾𝖺𝖽𝗆𝖾.𝗆𝖽 𝖡𝗒 𝗏𝗂𝗌𝗂𝗍𝗂𝗇𝗀 𝖬𝗒 𝖦𝗂𝗍𝗁𝗎𝖻 𝗅𝗂𝗇𝗄."
+echo " "
+echo " "
+echo -e "$ylo You can exit from this by cnt+c"
+echo " "
+2)echo " "
+echo " "
+echo " "
+echo -e "$red You are going to$grn Start$red ReTwitterBot$rset "
+echo " "
+echo -e "$gr Press$ylo ENTER$grn to continue$rset"
+echo " "
+read nitro
+cd $HOME/ReTwitterBot
+echo "$yellow +~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~ "
+sleep 3.50
+echo " "
+echo  $p 'Please Wait ===+['$b'./                   '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'..\                  '$p']|'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.../                 '$p']/'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'....\                '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'...../               '$p']|'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'......./             '$p']/'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........\            '$p']|'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'........../          '$p']\'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'..........\          '$p']/'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']|'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'..............\      '$p']/'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'............./       '$p']\'
+sleep 0.6
+clear
+echo  $p 'Please Wait ===+['$b'...............\     '$p']|'
+sleep 0.5
+clear
+echo  $p 'Please Wait ===+['$b'................/    '$p']/'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................\   '$p']\'
+sleep 0.4
+clear
+echo  $p 'Please Wait ===+['$b'.................../ '$p']|'
+sleep 0.4
+echo " "
+ls
+python3 Retweet.py
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+
+3)echo " "
+echo " "
+echo " "
+echo -e "$red You are going to$grn Update$red ReTwitterBot$rset "
+echo " "
+echo -e "$grn Press$ylo ENTER$grn to continue$rset"
+echo " "
+read mon
+cd $HOME/ReTwitterBot
+ls 
+chmod +x *
+bash update.sh
+echo " "
+echo " "
+echo -e "$cyan click$grn ENTER$cyan to go back into ReTwitterBot menu$rset"
+echo " "
+read punch
+cd $HOME/ReTwitterBot
+ls
+chmod +x *
+bash install.sh ;;
+4)echo " "
+echo " "
+echo " "
+echo -e "$red You are going to$grn delete$red ReTwitterBot$rset "
+echo " "
+echo -e "$grn Press$ylo ENTER$grn to continue$rset"
+echo " "
+read gzil
+cd $HOME/ReTwitterBot
+ls
+chmod +x *
+bash delete.sh
+echo " "
+echo " "
+cd $HOME ;;
+5)echo " "
+echo " "
+cd $HOME/ReTwitterBot
+ls
+bash about.sh
+echo " "
+echo " "
+echo -e "$cyan click$grn ENTER$cyan to go back into m-wiz menu$rset"
+echo " "
+read punch
+cd $HOME/ReTwitterBot
+bash install.sh ;;
+6)echo " "
+sleep 2.0
+clear
+echo " "
+echo -e "$grn
+                       ____  _  _  ____    ____  _  _  ____ 
+                      (  _ \( \/ )( ___)  (  _ \( \/ )( ___)
+                      ) _ < \  /  )__)    ) _ < \  /  )__) 
+                      (____/ (__) (____)  (____/ (__) (____)...$cyan have a nice day$rset"
+echo " "
+echo " "
+exit
+esac
