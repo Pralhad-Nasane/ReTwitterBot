a6c6c4d7742f6e883d6e874e877ef0fae14d6299
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-03 22:26:32 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 # Twitter application consumer keys and access tokens.
 # Remember, keep this information private.
-consumer_key = 'Enter your Keys'
-consumer_secret = 'Enter Your Keys'
-access_token = 'Enter Your Keys'
-access_token_secret = 'Enter your keys '
+consumer_key = 'rk5w32TV5dpFasyR4sK19znqW'
+consumer_secret = 'fCYyE2rCVFbhK2BVLY7SzH9GY9W3jJBtwUX6nRrBoQ8RL4Abm0'
+access_token = '724565930823561217-jvqG1oZKYPvF7jx9HatOkPHKgLrYrna'
+access_token_secret = 'UZ21N583rtu5937uneA2xNmbeIy0D2iteCkQmfrm7y5UN'
