087a3a11a4a895cb25aa9f1c406844e07a566fcc
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-03 14:39:37 +0530

    Update Retweet.py

--- Retweet.py
+++ Retweet.py
@@ -7,7 +7,7 @@ from keys import *
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-
+Hastagfile = print(Enter your hastag which you whant to retweet for this type)
 # Where q='#example', change #example to whatever hashtag or keyword you want to search.
 # Where items(5), change 5 to the amount of retweets you want to tweet.
 # Make sure you read Twitter's rules on automation - don't spam!
