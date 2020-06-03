dd820dc36df6a493cebc23e3a04650635d231ba3
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-03 16:16:37 +0530

    Update query.py

--- query.py
+++ query.py
@@ -1,3 +1,20 @@
 print(Hello Friends Please only edit hastags and no you want to retweet)
 for tweet in tweepy.Cursor(api.search, q=('#java OR #nasa OR #Aurdino -filter:retweets'), lang='en').items(500):
   try:
+       print('\nRetweet Bot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+
+        tweet.retweet()
+        print('Retweet published successfully.')
+
+        # Where sleep(10), sleep is measured in seconds.
+        # Change 10 to amount of seconds you want to have in-between retweets.
+        # Read Twitter's rules on automation. Don't spam!
+        sleep(2)
+
+    # Some basic error handling. Will print out why retweet failed, into your terminal.
+    except tweepy.TweepError as error:
+        print('\nError. Retweet not successful. Reason: ')
+        print(error.reason)
+
+    except StopIteration:
+        break
