1711685e2d9b2b01b609a2ff28bebfa75f27c236
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-03 20:18:56 +0530

    Create delete.sh

---
+++ delete.sh
@@ -0,0 +1,32 @@
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                          _      _      _       
+                         | |    | |    | |      
+                       __| | ___| | ___| |_ ___ 
+                      / _` |/ _ \ |/ _ \ __/ _ \
+                     | (_| |  __/ |  __/ ||  __/
+                      \__,_|\___|_|\___|\__\___|
+                           
+' | lolcat
+echo " "
+echo " "
+echo -e "$grn  Hello buddy If You Like My Tool Then Follow Me On Social Sites$rset"
+sleep 2.0
+echo " "
+echo " "
+cd $HOME
+rm -rf ReTwitterBot
+clear
+echo " "
+echo " "
+clear
+cd $HOME
+ls
