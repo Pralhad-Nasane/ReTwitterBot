275fa6b46cc79237d5d5dda60b6d292b64334102
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-04 10:32:02 +0530

    Update about.sh

--- about.sh
+++ about.sh
@@ -10,22 +10,23 @@ echo '
                                                  
 ' | lolcat
 echo " "
-echo "                         About"|lolcat
+echo "                        About"|lolcat
 echo " "
-echo "       🙏 Hey, there I am Pralhad, i made this tool
-            to install and use TwitterBot in termux become 
-    with the help of this tool you can retweet tweets without searching 
-         hastags for retweet, so i hope guys you liked it. 😘"
+echo "     🙏 Hey, there I am Pralhad, i made this tool
+          to install and use TwitterBot in termux become 
+           with the help of this tool you can retweet
+          tweets without searching hastags for retweet, 
+               so i hope guys you liked it. 😘"
 echo ""
-echo "                        About Me 👇🏻
+echo "                         About Me 👇🏻
 
-                            Ꭲꭼꮯꮋ-🇳ꭼꭱꭰ😉
+                             Ꭲꭼꮯꮋ-🇳ꭼꭱꭰ😉
                       
-                    💻 ͝ʹ͜˻͝∙͜͡˓K͜͝n͜͝o͜͝w͜͝ L͜͝a͜͝n͜͝g͜͝u͜͝a͜͝g͜͝e͜͝ ˓̇͜∙͡ʹ͜˻͝ ِ 👇🏻
+                     💻 ͝ʹ͜˻͝∙͜͡˓K͜͝n͜͝o͜͝w͜͝ L͜͝a͜͝n͜͝g͜͝u͜͝a͜͝g͜͝e͜͝ ˓̇͜∙͡ʹ͜˻͝ ِ 👇🏻
            
-                           👨🏻‍💻 > 𝘾
-                           👨🏻‍💻 > 𝘾++
-                           👨🏻‍💻 > 𝙋𝙮𝙩𝙝𝙤𝙣 
-                           👨🏻‍💻 > 𝙅𝙖𝙫𝙖
-                     🌐 𝐊𝐧𝐨𝐰𝐥𝐞𝐝𝐠𝐞 𝐎𝐟 𝐍𝐞𝐭𝐰𝐨𝐫𝐤𝐢𝐧𝐠 🌐"| lolcat
+                            👨🏻‍💻 > 𝘾
+                            👨🏻‍💻 > 𝘾++
+                            👨🏻‍💻 > 𝙋𝙮𝙩𝙝𝙤𝙣 
+                            👨🏻‍💻 > 𝙅𝙖𝙫𝙖
+                      🌐 𝐊𝐧𝐨𝐰𝐥𝐞𝐝𝐠𝐞 𝐎𝐟 𝐍𝐞𝐭𝐰𝐨𝐫𝐤𝐢𝐧𝐠 🌐"| lolcat
 echo " "
