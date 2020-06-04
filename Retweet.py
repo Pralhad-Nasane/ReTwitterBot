08075ab2f1ff3e3201d35f6dcb058d931df52716
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-04 14:37:01 +0530

    Update Retweet.py

--- Retweet.py
+++ Retweet.py
@@ -15,13 +15,15 @@ api = tweepy.API(auth)
 # Where q='#example', change #example to whatever hashtag or keyword you want to search.
 # Where items(5), change 5 to the amount of retweets you want to tweet.
 # Make sure you read Twitter's rules on automation - don't spammer.
-print('
+
 print('______       _     _____ _             _           _            ')          
 print('| ___ \     | |   /  ___| |           | |         | |  _        ')       
 print('| |_/ / ___ | |_  \ `--.| |_ __ _ _ __| |_ ___  __| | (_)______ ')
 print('| ___ \/ _ \| __|  `--. \ __/ _` | '__| --/ _ \/ _` |   |______|')
 print('| |_/ / (_) | |_  /\__/ / || (_| | |  | ||  __/ (_| |  _        ')       
 print('\____/ \___/ \__| \____/ \__\__,_|_|   \__\___|\__,_| (_)       ')
+      
+      
 for tweet in tweepy.Cursor(api.search, q=('#NASA OR #ISRO -filter:retweets'), lang='en').items(5):
     try:
        
