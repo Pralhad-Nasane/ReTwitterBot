cc63b28f949c47bd2206de3de4537d97298ed9f7
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-04 19:24:42 +0530

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,3 +1,11 @@
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#Don't Change It Plz 
 echo " "
 echo -e "$grn I Hope So That You Already Created Your Developer Account As Per Instructions
         If Not Then Go To My Github Link And Read All Procedures  "
