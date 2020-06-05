2ffd48dfa7fd52d43404fdac543b59624a63b539
Pralhad Nasane <63730055+Pralhad-Nasane@users.noreply.github.com>
2020-06-05 16:00:27 +0530

    Update Retweet.py

--- Retweet.py
+++ Retweet.py
@@ -15,16 +15,16 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 #########################################################################################
 # Where q='#example', change #example to whatever hashtag or keyword you want to search.#
-# Where items(5), change 5 to the amount of retweets you want to tweet.                 #
+# Where items(10), change 10 to the amount of retweets you want to tweet.                 #
 # Make sure you read Twitter's rules on automation - don't spammer.                     #
 #########################################################################################
-for tweet in tweepy.Cursor(api.search, q=('#NASA OR #ISRO -filter:retweets'), lang='en').items(5):
+for tweet in tweepy.Cursor(api.search, q=('#NASA OR #ISRO -filter:retweets'), lang='en').items(10):
     try:
        
-        print(Fore.YELLOW'\nTweet by: @' + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
+        print(Fore.YELLOW + '\nTweet by: @' Fore.BLUE + tweet.user.screen_name + '. ' + 'Attempting to retweet.')
         
         tweet.retweet()
-        print(Fore.BLUE'Retweeted the tweet')
+        print(Fore.BLUE + 'Retweeted the tweet')
 
     except tweepy.TweepError as e:
         print(e.reason)
